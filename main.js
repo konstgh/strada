99dd1910f30dfa1b019e45f39cb512cf78b0ee77
konstgh <const.targeting@gmail.com>
2022-10-10 12:03:23 +0700

    commit

---
+++ main.js
@@ -0,0 +1,51 @@
+// let user = 'John';
+// let age = 25;
+// let menoesage = 'Hello'; // определяем переменную и присваиваем ей значение
+
+// alert(message) // показывает содержимое переменной
+
+// let hello = 'Hello world!';
+
+// let message;
+
+// // копируем значение 'Hello world' из переменной hello в переменную message
+// message = hello;
+
+// // теперь две переменные содержат одинаковые данные
+// alert(hello); // Hello world!
+// alert(message); // Hello world!
+
+// let $ = 1; // объявили переменную с именем "$"
+// let _ = 2; // а теперь переменную с именем "_"
+
+// alert($ + _); // 3
+
+
+
+
+
+
+// let admin;
+// let name = 'Джон';
+
+// admin = name;    
+
+
+
+
+// let name = "Иван";
+
+// // Вставим переменную
+// alert( `Привет, ${name}!` ); // Привет, Иван!
+
+// // Вставим выражение
+// alert( `результат: ${1 + 2}` ); // результат: 3
+
+
+let value = true;
+alert (typeof value); 
+
+
+// console.log(typeof value)
+
+
